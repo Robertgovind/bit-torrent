acca85118426986f467f126b3f2c772f3d31ef7d
Goivnd Kumar Yadav <robertgovind@gmail.com>
2024-07-30 19:47:38 +0545

    bencode decode

--- app/main.py
+++ app/main.py
@@ -3,18 +3,22 @@ import sys
 import bencodepy
 # import bencodepy - available if you need it!
 # import requests - available if you need it!
-bc = bencodepy.Bencode(encoding="utf-8")
+
+bc = bencodepy.Bencode(encoding='utf-8')
 # Examples:
 #
 # - decode_bencode(b"5:hello") -> b"hello"
 # - decode_bencode(b"10:hello12345") -> b"hello12345"
+
 def decode_bencode(bencoded_value):
-    return bencodepy.decode(bencoded_value)
     return bc.decode(bencoded_value)
+        
 def main():
     command = sys.argv[1]
+
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
+
         # json.dumps() can't handle bytes, but bencoded "strings" need to be
         # bytestrings since they might contain non utf-8 characters.
         #
@@ -22,9 +26,13 @@ def main():
         def bytes_to_str(data):
             if isinstance(data, bytes):
                 return data.decode()
+
             raise TypeError(f"Type not serializable: {type(data)}")
+
         print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
+
+
 if __name__ == "__main__":
     main()
